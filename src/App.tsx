67cf837b5662bba784b1e40a4ced5cbb9fc71835
danangamw <danangamw@gmail.com>
2023-04-02 09:55:56 +0700

    initialize notes app, create layout

---
+++ src/App.tsx
@@ -0,0 +1,56 @@
+import { useEditor, EditorContent } from '@tiptap/react';
+import StarterKit from '@tiptap/starter-kit';
+import styles from './App.module.css';
+
+function App() {
+  const editor = useEditor({
+    extensions: [StarterKit],
+    content: '<p>Hello World!</p>',
+    editorProps: {
+      attributes: {
+        class: styles.textEditor,
+      },
+    },
+  });
+
+  const toggleBold = () => {
+    editor?.chain().focus().toggleBold().run();
+  };
+
+  const toggleItalic = () => {
+    editor?.chain().focus().toggleItalic().run();
+  };
+
+  return (
+    <section className={styles.pageContainer}>
+      <div className={styles.sidebar}>Sidebar</div>
+      <div className={styles.editorContainer}>
+        <div className={styles.toolbar}>
+          <button
+            className={
+              editor?.isActive('bold')
+                ? styles.toolbarButtonActive
+                : styles.toolbarButton
+            }
+            onClick={toggleBold}
+          >
+            Bold
+          </button>
+          <button
+            className={
+              editor?.isActive('italic')
+                ? styles.toolbarButtonActive
+                : styles.toolbarButton
+            }
+            onClick={toggleItalic}
+          >
+            Italic
+          </button>
+        </div>
+        <EditorContent editor={editor} className={styles.textEditorContent} />
+      </div>
+    </section>
+  );
+}
+
+export default App;
